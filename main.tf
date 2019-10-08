61f8e5faa8dfa15d1cf2115ebca5099737d80d29
Atanas Chuchulev <42144722+achuchulev@users.noreply.github.com>
2019-10-08 09:43:55 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+providers {
+  extip = ["1.0"]
+}
+
+data "extip" "external_ip" {}
+
+
+resource "null_resource" "print-exip" {
+  provisioner "local-exec" {
+    command = "echo Hello world ${data.extip.external_ip.ipaddress}!"
+  }
+}
